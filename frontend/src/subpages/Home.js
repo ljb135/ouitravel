2fb198a7ee7d0b29d8958efb0a41f67e5ecec8ea
Chris Liang <christopherliang.cl@gmail.com>
2023-03-30 21:13:52 -0400

    Changed dashboard formatting

--- frontend/src/subpages/Home.js
+++ frontend/src/subpages/Home.js
@@ -5,7 +5,7 @@ import { Card, Container, Row, Col, Stack, ListGroup, Badge } from 'react-bootst
 function Home() {
   return (
     <Container>
-      <h1 className='m-3'>Your trips</h1>
+      <h1 className='my-4'>Your trips</h1>
       <Stack direction="horizontal" gap={3}>
         <Card>
           <Card.Body>
@@ -13,11 +13,18 @@ function Home() {
             <Card.Subtitle className='mb-2 text-muted'>8/12/2023 - 3/30/2024</Card.Subtitle>
           </Card.Body>
           <ListGroup className="list-group-flush">
-            <ListGroup.Item>Flights
-            <Badge bg="secondary">New</Badge>
+            <ListGroup.Item className="d-flex justify-content-between">
+              Flights
+              <Badge bg="success">✓</Badge>
             </ListGroup.Item>
-            <ListGroup.Item>Hotels ✖</ListGroup.Item>
-            <ListGroup.Item>Attractions ✖</ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between">
+              Hotels
+              <Badge bg="danger">✕</Badge>
+            </ListGroup.Item>
+            <ListGroup.Item className="d-flex justify-content-between">
+              Activities
+              <Badge bg="secondary">0</Badge>
+            </ListGroup.Item>
           </ListGroup>
           <Card.Footer>Created by User1</Card.Footer>
         </Card>
