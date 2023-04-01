2bc7c56fa1b365121c5bb8f472ed690f074225d7
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-04-01 15:47:33 -0400

    a

--- frontend/src/subpages/Paypal.js
+++ frontend/src/subpages/Paypal.js
@@ -1,6 +1,8 @@
 function Paypal() {
     return (
       <div className="Paypal">
+        <script src="https://www.paypal.com/sdk/js?client-id=AYvk_kMZLe9ZaOrBQSKBVpH3mA8zD2A7GRwWAaf9CqK6OEnL9bzu_x_N7F-2699ylU3skfWKgyX2ZugP&currency=USD"></script>
+        <div id="paypal-button-container"></div>
         <h1>This is a new page</h1>
       </div>
     );
