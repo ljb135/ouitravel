4b2814379b305f760b43dc00bfcd9912013f2b95
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-04-24 18:18:45 -0400

    Updated friend system backend + frontend

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -1,8 +1,9 @@
 const express = require('express'), router = express.Router();
 const Friends = require('../models/friends');
+const User = require('../models/user');
 
 //FRIEND SYSTEM API CALLS
-
+/*
 //Show list of friends of user
 router.get('/friends', async (req, res) => {
   if (req.user) {
@@ -23,13 +24,106 @@ router.get('/friends', async (req, res) => {
     res.status(401).send('Not logged in');
   }
 });
+*/
+//Show list of friends of user
+router.get('/friends', async (req, res) => {
+  if (req.user) {
+    const email = req.user.email;
+    const status = req.query.status;
+    const key = req.query.key;
+
+    // For friends
+    if (status == "friends"){
+      Friends.find({ $and: [{ "status": status }, { $or: [{ "user1_email": email }, { "user2_email": email }] }] })
+      .then(data => {
+        data = data.map(friend => {
+          friend.user2_email = friend.user1_email === email ? friend.user2_email : friend.user1_email;
+          friend.user1_email = email;
+          return friend
+        })
+        console.log(data)
+        res.json(data);
+      })
+      .catch(error => res.json(error))
+    } 
+    // For sent requests
+    else if (status == "pending" && key == "sent"){
+      Friends.find({ $and: [{ "status": status }, { "user1_email": email }] })
+      .then(data => {
+        data = data.map(friend => {
+          friend.user2_email = friend.user1_email === email ? friend.user2_email : friend.user1_email;
+          friend.user1_email = email;
+          return friend
+        })
+        console.log(data)
+        res.json(data);
+      })
+      .catch(error => res.json(error))
+    } 
+    // For received requests
+    else if (status == "pending" && key == "received"){
+      Friends.find({ $and: [{ "status": status }, { "user2_email": email }] })
+      .then(data => {
+        data = data.map(friend => {
+          friend.user2_email = friend.user1_email === email ? friend.user2_email : friend.user1_email;
+          friend.user1_email = email;
+          return friend
+        })
+        console.log(data)
+        res.json(data);
+      })
+      .catch(error => res.json(error))
+    } 
+  }
+  else {
+    res.status(401).send('Not logged in');
+  }
+});
   
 //Add a new pending friend request between user and entered friend
 router.post('/friends', async (req, res) => {
   if (req.user) {
     const user1_email = req.user.email;
-    const user2_email = req.body.friend_email;
+    let user2_email = req.body.friend_email;
     const status = "pending";
+    
+    //Check if inputed friend email is an existing user
+    try {
+      let existingUser = await User.findOne({ 'email': req.body.friend_email });
+      if (existingUser) {
+        Friends.create({
+          user1_email,
+          user2_email,
+          status
+        });
+        res.send("Friend request sent");
+      }
+    } catch (error) {
+      return res.status(400).send(error); //res.status(401).send('Nonexisting user');
+    }
+    
+    /*
+    try {
+      // console.log(profile)
+      let existingUser = await User.findOne({ 'email': profile.email });
+      if (existingUser) {
+          return done(null, existingUser);
+      }
+      console.log('Creating new user...');
+      const newUser = new User({
+          first_name: profile.given_name,
+          last_name: profile.family_name,
+          email: profile.email,
+          dob: Date(),
+          is_mod: false
+      });
+      await newUser.save();
+      return done(null, newUser);
+  } catch (error) {
+      return done(error, false)
+  }
+    */
+    /*
     try {
       Friends.create({
         user1_email,
@@ -41,6 +135,7 @@ router.post('/friends', async (req, res) => {
       console.log(error)
       return res.json({ status: 'error' })
     }
+    */
   }
   else {
     res.status(401).send('Not logged in');
--- frontend/src/subpages/Friends.js
+++ frontend/src/subpages/Friends.js
@@ -8,15 +8,22 @@ import { useParams } from 'react-router-dom';
 function Friends(props) {
     const FriendParams = useParams();
     const [friends, setFriends] = useState(null);
+    const [sent, setSent] = useState(null);
+    const [received, setReceived] = useState(null);
     const [user1Email, setUser1Email] = useState("");
     const [user2Email, setUser2Email] = useState("");
     const [stat, setStatus] = useState();
     const [target_email, setTargetEmail] = useState("");
 
-    
-    function getFriends() {
+    //Get friends list
+    function getFriends(status) {
+
+        const query = new URLSearchParams({
+            status: "friends"
+        });
+
         const fetchData = async() => {
-            const response = await fetch('http://localhost:3001/friends', {
+            const response = await fetch('http://localhost:3001/friends?' + query, {
                 method: 'GET',
                 redirect: 'follow',
                 credentials: "include"
@@ -33,36 +40,56 @@ function Friends(props) {
     }, []);
 
     console.log(friends);
-
-    // Adding a friend into a list
-    function postFriends(event) {
-        event.preventDefault();
-
-
-        const submitFriends = async() => {
-
-            const postRes = await fetch('http://localhost:3001/addfriend', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    user1_email: user1Email,
-                    user2_email: user2Email,
-                    status: true
-                }),
-                'credentials': 'include'
-            });
-
-            const postResJson = await postRes.json()
-            setFriends(postResJson)
-        }
-
-        submitFriends();
-        getFriends();
-    }
     
-    // useEffect(() => {
-    //     postFriends();
-    // }, []);
+    //Get received requests
+    function getReceived() {
+
+        const query = new URLSearchParams({
+            status: "pending",
+            key: "received"
+        });
+
+        const fetchData = async() => {
+            const response = await fetch('http://localhost:3001/friends?' + query, {
+                method: 'GET',
+                redirect: 'follow',
+                credentials: "include"
+            })
+            const jsonRes = await response.json();
+
+            setReceived(jsonRes)
+        }
+        fetchData();
+    }
+
+    useEffect(() => {
+        getReceived();
+    }, []);
+
+    //Get sent requests
+    function getSent() {
+
+        const query = new URLSearchParams({
+            status: "pending",
+            key: "sent"
+        });
+
+        const fetchData = async() => {
+            const response = await fetch('http://localhost:3001/friends?' + query, {
+                method: 'GET',
+                redirect: 'follow',
+                credentials: "include"
+            })
+            const jsonRes = await response.json();
+
+            setSent(jsonRes)
+        }
+        fetchData();
+    }
+
+    useEffect(() => {
+        getSent();
+    }, []);
 
     // Accpeted a friend (update)
     function putFriends(event) {
@@ -110,22 +137,15 @@ function Friends(props) {
     //     deleteFriends();
     // }, []);
 
-    let items;
-
-    if(friends !== null){
-        items = friends.map((friend) => <FriendItem friend={friend}/>);
-        // items = [<FriendItem/>, <FriendItem/>, ...]
-    }
-
     // Send Friend Request
-    function handleSubmit(e) {
+    function handleFriendRequest(e) {
         // Prevent the browser from reloading the page
         e.preventDefault();
-
+        
         // Set body using inputted target_email
         const body = new URLSearchParams({
             friend_email: target_email
-          });
+        });
 
         // Use body in fetch 'POST' call to /friends:
         fetch('http://localhost:3001/friends', { 
@@ -139,40 +159,98 @@ function Friends(props) {
                 alert(`Friends request sent to ${target_email}`);
             }
             else{
-                alert(`Could not send friend request`);
+                //alert(`Could not send friend request: ${target_email}`);
             }
+        }).catch(error => {
+            alert(`Could not send friend request: ${target_email}`);
         });
+    }
+
+    // Map friends list
+    let items;
+    if(friends !== null){
+        items = friends.map((friend) => <FriendItem friend={friend}/>);
+        // items = [<FriendItem/>, <FriendItem/>, ...]
+    }
+
+    // Map received requests
+    let receivedItems;
+    if(received !== null){
+        receivedItems = received.map((friend) => <ReceivedItem friend={friend}/>);
+    }
+
+    // Map sent requests
+    let sentItems;
+    if(sent !== null){
+        sentItems = sent.map((friend) => <SentItem friend={friend}/>);
     }
 
     return (
         <Container>
             <h1>Friends list</h1>
-            <h2>Friends</h2>
             
-            <form method="POST" onSubmit={handleSubmit}>
-                <label>Friend's email: 
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <form method="POST" onSubmit={handleFriendRequest}>
                     <input 
-                        type="text" 
+                        type="text" placeholder="Friend's email"
                         value={target_email}
                         onChange={(e) => setTargetEmail(e.target.value)}
                     />
-                </label>
-                <button type="submit">Send friend request</button>
-            </form>
+                    <button type="submit">Send friend request</button>
+                </form>
+            </div>
 
+            <h2>Friends</h2>
             <ListGroup>
                 {items}
             </ListGroup>
             <button> Delete All</button>
 
-
-            <h2>Pending</h2>
+            <h2>Received Requests</h2>
+            <ListGroup>
+                {receivedItems}
+            </ListGroup>
             <button>Accept All</button>
+
+            <h2>Sent Requests</h2>
+            <ListGroup>
+                {sentItems}
+            </ListGroup>
         </Container>
     );
 }
 
 function FriendItem({friend}){
+    return(
+        <ListGroup.Item>
+            {friend.user2_email}
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <button>
+                Remove
+                </button>
+            </div>
+        </ListGroup.Item>
+    )
+}
+
+function handleAccept(){
+    
+}
+
+function ReceivedItem({friend}){
+    return(
+        <ListGroup.Item>
+            {friend.user2_email}
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <button onClick={handleAccept}>
+                Delete
+                </button>
+            </div>
+        </ListGroup.Item>
+    )
+}
+
+function SentItem({friend}){
     return(
         <ListGroup.Item>
             {friend.user2_email}
